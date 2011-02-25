2d2b5d4ff381904e77e67487dd91643177d31bfb
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-02-24 22:58:47 -0500

    Deleted an unnecessary file

--- src/block.py
+++ src/block.py
@@ -1,67 +1,26 @@
-
 class Block():
     def __init__(self):
-        self.accounts = {}  # dictionary indexed by accountid storing accounts
+        self.locked = False
 
-    def openaccount(self, args):
-        accountid = args[0]
-        if self.accounts.has_key(accountid):
+    def lock(self, args):
+        if self.locked == True:
             return "FAILURE"
         else:
-            self.accounts[accountid] = Account(accountid)
-            return "ACCT %s OPENED" % accountid
+            self.locked = True
+            return "block locked"
         
-    def closeaccount(self, args):
-        accountid = args[0]
-        if self.accounts.has_key(accountid):
-            del self.accounts[accountid]
-            return "ACCT %s CLOSED" % accountid
-        else:
-            return "FAILURE"
+    def trylock(self, args):
+        pass
         
-    def debit(self, args):
-        accountid = args[0]
-        if self.accounts.has_key(accountid):
-            self.accounts[accountid].debit()
-            return "DEBIT SUCCESSFUL: %.2f" % self.accounts[accountid].balance
-        else:
-            return "FAILURE"
+    def unlock(self, args):
+        self.locked = False
+        return "block unlocked"
         
-    def deposit(self, args):
-        accountid = args[0]
-        if self.accounts.has_key(accountid):
-            self.accounts[accountid].deposit()
-            return "DEPOSIT SUCCESSFUL: %.2f" % self.accounts[accountid].balance
-        else:
-            return "FAILURE"
-        
-    def balance(self, args):
-        accountid = args[0]
-        if self.accounts.has_key(accountid):
-            return "CURRENT BALANCE: %.2f" % self.accounts[accountid].balance
-        else:
-            return "FAILURE"
+    def state(self, args):
+        return self.lock
     
     def __str__(self):
-        temp = "**Bank**\n"
-        for account in self.accounts.values():
-            temp += str(account)+"\n"
-        return temp
-
-class Account():
-    def __init__(self,id):
-        self.id = id
-        self.balance = 100
-        self.commands = {} # dictionary indexed by commandnumber storing commands
-        
-    def __str__(self):
-        return "Account %s: balance = %.2f" % (self.id, self.balance)
-    
-    def debit(self):
-        self.balance = self.balance*0.9
-        
-    def deposit(self):
-        self.balance = self.balance*1.1
+        return self.lock
         
     
         
