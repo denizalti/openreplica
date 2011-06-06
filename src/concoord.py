3cf2742cb594d57e889ba8b9ff7ec97d4e4b4ae7
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-06-06 17:54:59 -0400

    returnoutofband updated. Distributed Condition acquire and release updated.

--- src/concoord.py
+++ src/concoord.py
@@ -5,13 +5,12 @@ from connection import Connection, ConnectionPool
 from exception import *
 from enums import *
 
-def return_outofband(designated, owner, command, destinations):
-    for dest in destinations:
-        clientreply = ClientReplyMessage(MSG_CLIENTMETAREPLY, owner.me, replycode=CR_METAREPLY, inresponseto=command.clientcommandnumber)
-        destconn = owner.clientpool.get_connection_by_peer(dest)
-        if destconn.thesocket == None:
-            return
-        destconn.send(clientreply)
+def return_outofband(designated, owner, command):
+    clientreply = ClientReplyMessage(MSG_CLIENTMETAREPLY, owner.me, replycode=CR_METAREPLY, inresponseto=command.clientcommandnumber)
+    destconn = owner.clientpool.get_connection_by_peer(command.client)
+    if destconn.thesocket == None:
+        return
+    destconn.send(clientreply)
 
 class DistributedLock():
     def __init__(self):
@@ -35,15 +34,14 @@ class DistributedLock():
                     self.holder = None
                     self.locked = False
                 else:
-                    oldholder = self.holder
                     self.queue.reverse()
                     newcommand = self.queue.pop()
-                    self.holder = newcommand.client
                     self.queue.reverse()
+                    self.holder = newcommand.client
                     # return to old holder which made a release
-                    return_outofband(_concoord_designated, _concoord_owner, _concoord_command, [oldholder])
+                    return_outofband(_concoord_designated, _concoord_owner, _concoord_command)
                     # return to new holder which is waiting
-                    return_outofband(_concoord_designated, _concoord_owner, newcommand, [self.holder])
+                    return_outofband(_concoord_designated, _concoord_owner, newcommand)
                     raise UnusualReturn
         else:
             return "Release on unacquired lock"
@@ -58,42 +56,58 @@ class DistributedCondition():
         else:
             self.lock = Lock()
         self.locked = False
-        self.holder = None
-        self.queue = []
+        self.lockholder = None
+        self.lockqueue = []
         self.waiting = []
     
     def acquire(self, _concoord_designated, _concoord_owner, _concoord_command):
         if self.locked == True:
-            self.queue.append(_concoord_command)
+            self.lockqueue.append(_concoord_command)
             raise UnusualReturn
         else:
-            self.holder = _concoord_command.client
+            self.lockholder = _concoord_command.client
             self.locked = True
 
-    def release(self):
+    def release(self, _concoord_designated, _concoord_owner, _concoord_command):
         if self.locked == True and self.holder == _concoord_command.client:
             with self.lock:
-                if len(self.queue) == 0:
-                    self.holder = None
+                if len(self.lockqueue) == 0:
+                    self.lockholder = None
                     self.locked = False
                 else:
-                    oldholder = self.holder
-                    self.queue.reverse()
-                    newcommand = self.queue.pop()
-                    self.holder = newcommand.client
-                    self.queue.reverse()
+                    self.lockqueue.reverse()
+                    newcommand = self.lockqueue.pop()
+                    self.lockqueue.reverse()
+                    self.lockholder = newcommand.client
                     # return to old holder which made a release
-                    return_outofband(_concoord_designated, _concoord_owner, _concoord_command, [oldholder])
+                    return_outofband(_concoord_designated, _concoord_owner, _concoord_command)
                     # return to new holder which is waiting
-                    return_outofband(_concoord_designated, _concoord_owner, newcommand, [self.holder])
+                    return_outofband(_concoord_designated, _concoord_owner, newcommand)
                     raise UnusualReturn
         else:
             return "Release on unacquired lock"
 
-    def wait(self):
+    def wait(self, _concoord_designated, _concoord_owner, _concoord_command):
+        # put the caller on waitinglist and take the lock away
         self.waiting.append(caller)
-        return_outofband(_concoord_me, _concoord_client_cmdno, caller)
-        raise UnusualReturn
+        if self.locked == True and self.holder == _concoord_command.client:
+            with self.lock:
+                if len(self.lockqueue) == 0:
+                    self.lockholder = None
+                    self.locked = False
+                else:
+                    self.lockqueue.reverse()
+                    newcommand = self.lockqueue.pop()
+                    self.lockqueue.reverse()
+                    self.lockholder = newcommand.client
+                    # return to old holder which made a release
+                    return_outofband(_concoord_designated, _concoord_owner, _concoord_command)
+                    # return to new holder which is waiting
+                    return_outofband(_concoord_designated, _concoord_owner, newcommand)
+                    raise UnusualReturn
+        else:
+            return "Can't wait on unacquired condition"
+        
 
     def notify(self):
         self.waiting.reverse()
