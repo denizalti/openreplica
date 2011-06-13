fcec785556743e9a507a7425c2a1374be38c9f4c
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-06-12 21:29:56 -0400

    RLock and BoundedSemaphore added.

---
+++ src/threadingobj/boundedsemaphore.py
@@ -0,0 +1,36 @@
+from concoord import *
+from exception import *
+
+class DistributedBoundedSemaphore():
+    def __init__(self, count=1):
+        self.count = int(count)
+        self.queue = []
+        self.atomic = Lock()
+
+    def create(self, count=1):
+        self.count = int(count)
+        self.queue = []
+    
+    def acquire(self, kwargs):
+        _concoord_designated, _concoord_owner, _concoord_command = kwargs['_concoord_designated'], kwargs['_concoord_owner'], kwargs['_concoord_command']
+        with self.atomic:
+            self.count -= 1
+            if self.count < 0:
+                self.queue.append(_concoord_command)
+                raise UnusualReturn
+
+    def release(self, kwargs):
+        _concoord_designated, _concoord_owner, _concoord_command = kwargs['_concoord_designated'], kwargs['_concoord_owner'], kwargs['_concoord_command']
+        with self.atomic:
+            self.count += 1
+            if len(self.queue) > 0:
+                self.queue.reverse()
+                newcommand = self.queue.pop()
+                self.queue.reverse()
+                # return to new holder which is waiting
+                return_outofband(_concoord_designated, _concoord_owner, newcommand)
+                
+    def __str__(self):
+        temp = 'Distributed Semaphore'
+        temp += "\ncount: %d\nqueue: %s\n" % (self.count, " ".join([str(m) for m in self.queue]))
+        return temp
