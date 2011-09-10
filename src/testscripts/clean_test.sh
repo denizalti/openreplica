0856c6e18c53fa1709521563813f5a392f807f60
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-09-09 22:34:26 -0400

    These scripts are used to start, stop and clean the concoord tests from my local machine. rundiffmultiple runs different test multiple times. runsamemultiple runs the same test multiple times.
    The specifications (parameters etc.) are in the script files.

---
+++ src/testscripts/clean_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RUNFILE=./runningon
+
+if [[ ! -f ${RUNFILE} ]]
+then
+    echo "ConCoord RUNFILE doesn't exist".
+    exit 1
+fi
+
+a=0
+while read line
+do a=$(($a+1));
+machines[$a]=$line;
+done < ${RUNFILE}
+
+for (( i = 1; i <= a; i++ ))
+do
+    ssh ${machines[$i]} ./clean_concoord.sh
+done
+
+rm -rf ${RUNFILE}
