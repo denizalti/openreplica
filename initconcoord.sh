01dd8ddea4264bc7bc0a25fcbc95177f48fc927c
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-10-15 17:21:52 -0400

    scripts for syncing with syscluster are moved to paxi/updatesys

--- initconcoord.sh
+++
@@ -1,6 +0,0 @@
-rm -rf concoord
-tar -xvzf concoord.tar.gz
-mv src concoord
-chown deniz concoord/
-chown deniz concoord/*
-rm -rf concoord.tar.gz
--- initzookeeper.sh
+++
@@ -1,3 +0,0 @@
-rm -rf zookeeper
-tar -xvzf zookeeper.tar.gz
-rm -rf zookeeper.tar.gz
