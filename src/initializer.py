69765fe37d581d27b62c78dd4408b0cb64184ba4
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-11-08 20:22:21 -0500

    Safety Checker checks for imports and specific built-in functions. open (with w and a) setattr getattr compile execfile eval

---
+++ src/initializer.py
@@ -0,0 +1,52 @@
+'''
+@author: denizalti
+@note: initializer for openreplica
+@date: October 1, 2011
+'''
+from optparse import OptionParser
+
+parser = OptionParser(usage="usage: %prog -s subdomain -n objectname -o objectcode -r replicas")
+parser.add_option("-s", "--subdomain", action="store", dest="subdomain", help="name for the subdomain to reach openreplica")
+parser.add_option("-n", "--objectname", action="store", dest="objectname", help="client object name")
+parser.add_option("-o", "--objectcode", action="store", dest="objectcode", help="client object code")
+parser.add_option("-r", "--replicas", action="store", dest="replicanum", default=1, help="number of replicas")
+(options, args) = parser.parse_args()
+
+def main():
+    objectfile = create_object(options.objectname, options.objectcode)
+    objectsafe = check_object(objectfile)
+    if not objectsafe:
+        print "Object not safe. Terminating.."
+        return
+    start_system(objectname)
+    clientproxy = create_proxy()
+    return clientproxy
+
+def create_object(objectname, objectcode):
+    print "creating object.."
+    try: 
+        objectfile = open(objectname, "w")
+        objectfile.write(objectcode)
+        objectfile.close()
+        return True
+    except:
+        return False
+
+def check_object(objectfile):
+    print "checking object safety.."
+    # check imports
+    
+    return True
+
+def start_system(objectname):
+    print "starting system.."
+    
+def create_proxy(objectname):
+    print "creating proxy.."
+    proxyfile = createproxyfromname(objectname)
+    f = open(proxyfile.name, 'r')
+    proxystring = f.read()
+    return proxystring
+
+if __name__=='__main__':
+    main()
---
+++ src/safetychecker.py
@@ -0,0 +1,129 @@
+import compiler
+import ast, _ast
+import os
+from modulefinder import ModuleFinder
+from optparse import OptionParser
+
+parser = OptionParser(usage="usage: %prog -s subdomain -n objectname -o objectcode -r replicas")
+parser.add_option("-n", "--objectname", action="store", dest="objectname", help="client object name")
+parser.add_option("-o", "--objectcode", action="store", dest="objectcode", help="client object code")
+(options, args) = parser.parse_args()
+
+DEBUG = True
+
+class Visitor(ast.NodeVisitor):
+    def generic_visit(self, node):
+        print "---", ast.dump(node)
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_Import(self, node):
+        print "No imports allowed.. --> EXIT"
+        
+    def visit_Call(self, node):
+        if DEBUG:
+            print 'Call : '
+        self.check_functioncall(node)
+        self.generic_visit(node)
+
+    def visit_Name(self, node):
+        if DEBUG:
+            print 'Name :', node.id
+
+    def visit_Num(self, node):
+        if DEBUG:
+            print 'Num :', node.__dict__['n']
+
+    def visit_Str(self, node):
+        if DEBUG:
+            print "Str :", node.s
+
+    def visit_Print(self, node):
+        if DEBUG:
+            print "Print :"
+        self.generic_visit(node)
+
+    def visit_Assign(self, node):
+        if DEBUG:
+            print "Assign :"
+        self.generic_visit(node)
+
+    def visit_Expr(self, node):
+        if DEBUG:
+            print "Expr :"
+        self.generic_visit(node)
+
+    def addassignment(self, node):
+        global assignments
+        print "Adding assignment.."
+        for target in node.targets:
+            if type(target).__name__ == 'Name':
+               if type(node.value).__name__ == 'Name':
+                   assignments[target.id] = assignments[node.value.id]
+            elif type(target).__name__ == 'Attribute':
+                pass
+                
+        for fname,fvalue in ast.iter_fields(node):
+            if DEBUG:
+                print fname, " ", fvalue
+
+    def check_functioncall(self, node):
+        if DEBUG:
+            print "Checking function call.."
+        isopen=issetattr=isgetattr=iscompile=isexec=iseval= False
+        for fname,fvalue in ast.iter_fields(node):
+            if DEBUG:
+                print fname, " ", fvalue
+            #XXX: Do this in a more pythonic way
+            if type(fvalue).__name__ == 'Name':
+                #XXX: The Name doesn't have to be the original
+                #XXX: could be overwritten
+                if fvalue.id == 'open':
+                    isopen = True
+                elif fvalue.id == 'setattr':
+                    issetattr = True
+                elif fvalue.id == 'getattr':
+                    isgetattr = True
+                elif fvalue.id == 'compile':
+                    iscompile = True
+                elif fvalue.id == 'execfile':
+                    isexec = True
+                elif fvalue.id == 'eval':
+                    iseval = True
+            if fname == 'args' and type(fvalue).__name__ == 'Str':
+                for arg in fvalue:
+                    if arg.__dict__['s'] == 'w' and isopen:
+                        print "Writing to file.. --> EXIT"
+            if fname == 'args' and type(fvalue).__name__ == 'Name':
+                for arg in fvalue:
+                    if arg.__dict__['n'] == 'w' and isopen:
+                        print "Writing to file.. --> EXIT"
+        os._exit(0)
+def main():
+    path = "/Users/denizalti/paxi/src/obj/bank.py"
+    astnode = compile(open(path, 'rU').read(),"<string>","exec",_ast.PyCF_ONLY_AST)
+
+    v = Visitor()
+    v.visit(astnode)
+
+    #print astnode.body[0].names[0].name
+    #for child in ast.iter_child_nodes(astnode):
+    #    print ast.dump(child)
+
+    #astnode = compiler.parse(open(path, 'rU').read())
+    #print astnode.getChildren()
+    #print str(astnode)
+
+def check_imports(path):
+    finder = ModuleFinder()
+    finder.run_script(path)
+    modules = []
+    print 'Loaded modules:'
+    for name, mod in finder.modules.iteritems():
+        print '%s ' % name
+        if name != "__main__":
+            return False
+    return True
+
+if __name__=='__main__':
+    main()
+
