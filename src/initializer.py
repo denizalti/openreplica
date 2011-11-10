822ba84db4298b90336211e57a3a8e4e7a11ba11
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-11-10 00:19:36 -0500

    Initializer code is being updated.

--- src/initializer.py
+++ src/initializer.py
@@ -3,7 +3,9 @@
 @note: initializer for openreplica
 @date: October 1, 2011
 '''
+import os
 from optparse import OptionParser
+from safetychecker import *
 
 parser = OptionParser(usage="usage: %prog -s subdomain -n objectname -o objectcode -r replicas")
 parser.add_option("-s", "--subdomain", action="store", dest="subdomain", help="name for the subdomain to reach openreplica")
@@ -24,8 +26,9 @@ def main():
 
 def create_object(objectname, objectcode):
     print "creating object.."
-    try: 
-        objectfile = open(objectname, "w")
+    try:
+        abspath = os.path.abspath(objectname)
+        objectfile = open(abspath, "w")
         objectfile.write(objectcode)
         objectfile.close()
         return True
@@ -34,8 +37,9 @@ def create_object(objectname, objectcode):
 
 def check_object(objectfile):
     print "checking object safety.."
-    # check imports
-    
+    astnode = compile(open(objectfile.name, 'rU').read(),"<string>","exec",_ast.PyCF_ONLY_AST)
+    v = Visitor()
+    v.visit(astnode)
     return True
 
 def start_system(objectname):
