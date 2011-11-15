94ec9fb861565961ecd4c6e3119ae8a19cb3c848
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-11-15 12:25:29 -0500

    PlanetLab initialization file.

---
+++ src/plinit.py
@@ -0,0 +1,52 @@
+import xmlrpclib
+import sys
+
+s = xmlrpclib.ServerProxy('https://www.planet-lab.org/PLCAPI/', allow_none=True)
+
+# Create an empty dictionary (XML-RPC struct)
+auth = {}
+# Specify password authentication
+auth['Username'] = 'denizalti@gmail.com'
+auth['AuthString'] = '11235813'
+auth['AuthMethod'] = 'password'
+
+authorized = s.AuthCheck(auth)
+if not authorized:
+    print 'Authorization failed!'
+
+def listallnodes():
+    print 'Getting all nodes!'
+    all_nodes = s.GetNodes(auth)
+    for node in all_nodes:
+        print node['hostname']
+
+def writeallnodestofile():
+    f = open("plnodes.txt", 'w')
+    all_nodes = s.GetNodes(auth)
+    for node in all_nodes:
+        f.write(node['hostname']+"\n")
+    f.close()
+
+def addnodes(nodes):
+    slice_id = s.GetSlices(auth, ['cornell_openreplica'], ['slice_id'])[0]['slice_id']
+    s.AddSliceToNodes(auth, slice_id, nodes)
+
+def shownodes():
+    node_ids = s.GetSlices(auth, ['cornell_openreplica'], ['node_ids'])[0]['node_ids']
+    node_hostnames = [node['hostname'] for node in s.GetNodes(auth, node_ids, ['hostname'])]
+    print node_hostnames
+
+def getallnodes():
+    all_nodes = s.GetNodes(auth)
+    nodenames = []
+    for node in all_nodes:
+        nodenames.append(node['hostname'])
+    return nodenames
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    sys.exit(main())
+
+
