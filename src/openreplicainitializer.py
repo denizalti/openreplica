4b591e34ab01798f59a167cbc7c800f674d32bfd
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-11-20 16:05:51 -0500

    Commit before NameServer Coordination Object related design changes.

--- src/openreplicainitializer.py
+++ src/openreplicainitializer.py
@@ -10,7 +10,6 @@ import ast, _ast
 import subprocess
 from proxygenerator import *
 from plmanager import *
-from openreplicacoordobj import *
 from safetychecker import *
 
 parser = OptionParser(usage="usage: %prog -s subdomain -n objectname -o objectcode -r replicas")
@@ -57,22 +56,31 @@ def start_nodes(subdomain, clientobjectfile, numreplicas):
     plconn = PLConnection()
     plconn.connect(numreplicas)
     # add nodes to open replica coordinator object
-    openreplicacoordobj = OpenReplicaCoord()
-    openreplicacoordobj.addsubdomain(subdomain)
-    openreplicacoordobj.addnodestosubdomain(subdomain, plconn.getHosts())
+    #openreplicacoordobj = OpenReplicaCoord()
+    #openreplicacoordobj.addsubdomain(subdomain)
+    #openreplicacoordobj.addnodestosubdomain(subdomain, plconn.getHosts())
+    nodes = {}
+    nodes[subdomain] = plconn.getHosts()
     print "Picked nodes: "
-    for node in openreplicacoordobj.nodes[subdomain]:
+    for node in nodes[subdomain]:
         print node
     # upload concoord bundle
     print "[4] uploading files"
     pathtoconcoordbundle = os.path.abspath("test.zip")
+    pathtodnstester = os.path.abspath("testdnsport.py")
+    pathtoshscript = os.path.abspath("plopenreplica.sh")
     print "bundle under ", pathtoconcoordbundle
     plconn.upload(pathtoconcoordbundle)
     # upload clientobject
     plconn.upload(clientobjectfile.name)
+    # upload testdnsport
+    plconn.upload(pathtodnstester)
+    # upload shellscript
+    plconn.upload(pathtoshscript)
     # initialize nodes
-    print "[5] initializing"
-    #plconn.executecommand("")
+    print "[5] trying to bind to DNS port"
+    plconn.executecommand("sudo python testdnsport.py")
+    print "[6] initializing"
     
 def create_proxy(objectfile, classname):
     print "[6] creating proxy"
---
+++ src/plopenreplica.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+tar -xvzf openreplica.tar.gz
+rm -rf openreplica.tar.gz
