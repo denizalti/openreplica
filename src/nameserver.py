34a7e853a2c86bd3b728077e8566e81d2c9343dd
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-11-20 13:19:38 -0500

    The nameserver keeps track of the view by being involved in METACOMMANDS and listens on the DNS port(53) and asnwers DNS queries:
    - A Query: Answers with IP addresses of Replicas, starting with the Leader.
    - TXT Query: Answers with IP addresses of Replicas, Acceptors, Nameservers and Trackers.

--- src/nameserver.py
+++ src/nameserver.py
@@ -19,8 +19,7 @@ try:
 except:
     logger("Install dnspython: http://www.dnspython.org/")
 
-DOMAIN = ['groups','openreplica','org','']
-RRTYPE = ['','A','NS','MD','MF','CNAME','SOA']
+RRTYPE = ['','A','NS','MD','MF','CNAME','SOA', 'MB', 'MG', 'MR', 'NULL', 'WKS', 'PTR', 'HINFO', 'MINFO', 'MX', 'TXT', 'RP']
 RRCLASS = ['','IN','CS','CH','HS']
 OPCODES = ['QUERY','IQUERY','STATUS']
 RCODES = ['NOERROR','FORMERR','SERVFAIL','NXDOMAIN','NOTIMP','REFUSED']
@@ -28,18 +27,15 @@ RCODES = ['NOERROR','FORMERR','SERVFAIL','NXDOMAIN','NOTIMP','REFUSED']
 class Nameserver(Tracker):
     """Nameserver keeps track of the connectivity state of the system and replies to
     QUERY messages from dnsserver."""
-    def __init__(self):
+    def __init__(self, domain):
         Tracker.__init__(self, nodetype=NODE_NAMESERVER, port=5000, bootstrap=options.bootstrap)
-        self.name = dns.name.Name(['test']+DOMAIN)
-        # XXX: How to keep this dictionary up-to-date
-        self.registerednames = {dns.name.Name(['test']+DOMAIN):'127.0.0.1'} # <name:nameserver> mappings
-        self.nameserverconnections = {}  # <nameserver:connection> mappings
-        self.udpport = 53 #DNS port: 53
+        self.mydomain = dns.name.Name((domain+".").split("."))
+        self.udpport = 53
         self.udpsocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         try:
             self.udpsocket.bind((self.addr,self.udpport))
         except socket.error as e:
-            print "Can't bind to UDP socket: ", e
+            print "Can't bind to UDP socket 53: ", e
 
     def startservice(self):
         """Starts the background services associated with a node."""
@@ -48,7 +44,6 @@ class Nameserver(Tracker):
         UDP_server_thread = Thread(target=self.udp_server_loop)
         UDP_server_thread.start()
         
-    # DNS Side
     def udp_server_loop(self):
         while self.alive:
             try:
@@ -65,6 +60,29 @@ class Nameserver(Tracker):
         self.udpsocket.close()
         return
 
+    def ismyname(self, name):
+        return name == self.mydomain
+
+    def ismysubdomain(self, name):
+        return name == self.mydomain
+        
+    def aresponse(self, question):
+        for gname in [NODE_LEADER, NODE_REPLICA]:
+            for address in self.groups[gname].get_addresses():
+                yield address
+
+    def nsresponse(self, question):
+        for address in self.groups[NODE_NAMESERVER].get_addresses():
+            yield address
+        
+    def txtresponse(self, question):
+        txtstr = ''
+        for groupname,group in self.groups.iteritems():
+            txtstr += node_names[groupname] + ' = '
+            for address in group.get_addresses():
+                txtstr += str(address) + ' '
+        return txtstr
+    
     def handle_query(self, data, addr):
         f = open("nameserverlog", 'a')
         query = dns.message.from_wire(data)
@@ -72,49 +90,34 @@ class Nameserver(Tracker):
         f.write(strftime("%Y-%m-%d %H:%M:%S")+'\n')
         for question in query.question:
             f.write("\nReceived Query for %s\n" % question.name)
-            if question.name.parent() != self.name.parent():
-                f.write("Format Error\n")
-                # Which Flags to set?
-                # QR : this is a response
-                # AA : I'm an authority as I know all nameservers in the system
-                # RD : *recursion* comes from the query
-                # RCODE : 1 (Format Error)
-                flags = QR + AA + RD + dns.rcode.FORMERR
-                response.flags = flags
-            elif question.name == self.name:
-                f.write("This is me\n")
-                # Which Flags to set?
-                # QR : this is a response
-                # AA : as this is me I'm an authority
-                # RD : *recursion* comes from the query
-                # RA ? Support for Recursion
-                # RCODE : 0 (No Error)
-                flagstr = 'QR AA RD'
-                answerstr = ''
-                for group in self.groups.values():
-                    for address in group.get_addresses():
+            print "Question Name: ", question.name
+            print "My Domain: ", self.mydomain
+            if question.rdtype in [dns.rdatatype.A, dns.rdatatype.TXT] and self.ismyname(question.name):
+                f.write("This is me %s" % str(question)) 
+                flagstr = 'QR AA RD' # response, authoritative, recursion
+                answerstr = ''    
+                # A Queries --> List all Replicas starting with the Leader
+                if question.rdtype == dns.rdatatype.A:
+                    for address in self.aresponse(question):
                         answerstr += self.create_answer_section(question, addr=address)
+                # TXT Queries --> List all nodes
+                elif question.rdtype == dns.rdatatype.TXT:
+                    answerstr += self.create_answer_section(question, txt=self.txtresponse(question))
                 responsestr = self.create_response(response.id,opcode=dns.opcode.QUERY,rcode=dns.rcode.NOERROR,flags=flagstr,question=question.to_text(),answer=answerstr,authority='',additional='')
                 print responsestr
                 response = dns.message.from_text(responsestr)
-            elif self.registerednames.has_key(question.name):
-                f.write("Forwarding\n")
-                # Which Flags to set?
-                # QR : this is a response
-                # RD : *recursion* comes from the query
-                # RA ? Support for Recursion
-                # RCODE : 0 (No Error)
-                flagstr = 'QR RD'
-                answerstr = self.create_answer_section(question, rrtype=dns.rdatatype.NS, name=str(question.name))
+            elif question.rdtype == dns.rdatatype.NS and self.ismyname(question.name):
+                f.write("This is for my name server %s" % str(question)) 
+                flagstr = 'QR AA RD' # response, authoritative, recursion
+                answerstr = ''    
+                for address in self.nsresponse(question):
+                    print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", address
+                    answerstr += self.create_answer_section(question, addr=address)
                 responsestr = self.create_response(response.id,opcode=dns.opcode.QUERY,rcode=dns.rcode.NOERROR,flags=flagstr,question=question.to_text(),answer=answerstr,authority='',additional='')
+                print responsestr
                 response = dns.message.from_text(responsestr)
             else:
                 f.write("Name Error\n")
-                # Which Flags to set?
-                # QR : this is a response
-                # AA : I'm an authority as I know all nameservers in the system
-                # RD : *recursion* comes from the query
-                # RCODE : 3 (Name Error)
                 flags = QR + AA + RD + dns.rcode.NXDOMAIN
                 response.flags = flags
         f.write( "\nRESPONSE:\n%s\n---\n" % response)
@@ -125,13 +128,13 @@ class Nameserver(Tracker):
         responsestr = "id %s\nopcode %s\nrcode %s\nflags %s\n;QUESTION\n%s\n;ANSWER\n%s\n;AUTHORITY\n%s\n;ADDITIONAL\n%s\n" % (str(id), OPCODES[opcode], RCODES[rcode], flags, question, answer, authority, additional)
         return responsestr
 
-    def create_answer_section(self, question, ttl='3600', rrclass=1, rrtype=1, addr='', name=''):
-        if rrtype == dns.rdatatype.A:
-            answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(addr))
-        elif rrtype == dns.rdatatype.NS:
-            answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(name))
+    def create_answer_section(self, question, ttl='3600', rrclass=1, addr='', name='', txt=None):
+        if question.rdtype == dns.rdatatype.A or dns.rdatatype.TXT:
+            answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[question.rdtype]), str(addr) if txt is None else '"%s"' % txt)
+        elif question.rdtype == dns.rdatatype.NS:
+            answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[question.rdtype]), str(name))
         return answerstr
-
+    
     def create_authority_section(self, question, ttl='3600', rrclass=1, rrtype=1, addr='', name=''):
         if rrtype == dns.rdatatype.A:
             authoritystr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(addr))
@@ -147,7 +150,7 @@ class Nameserver(Tracker):
         return additionalstr
 
 def main():
-    nameservernode = Nameserver()
+    nameservernode = Nameserver(options.dnsname)
     nameservernode.startservice()
     signal.signal(signal.SIGINT, nameservernode.interrupt_handler)
     signal.signal(signal.SIGTERM, nameservernode.terminate_handler)
