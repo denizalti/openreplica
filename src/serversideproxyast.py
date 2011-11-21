04111504c57ca5f60357853f76bb5603bb74c54b
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-11-21 16:14:09 -0500

    Server side objectediter using AST.
    It only adds **kwargs to functions defitions under the class that
    will be used as our object. It doesn't ass **kwargs if it is already
    present.
    
    This technique is cleaner and more professional than the string read
    and compare method used previously. So I'll edit the old proxygenerator
    to use AST similarly.
    
    The script expects to inputs: -m modulename -o objectname

---
+++ src/serversideproxyast.py
@@ -0,0 +1,75 @@
+import inspect, types, string
+import os, shutil
+import ast, _ast
+from optparse import OptionParser
+
+parser = OptionParser(usage="usage: %prog -m modulename -o objectname")
+parser.add_option("-m", "--modulename", action="store", dest="modulename", help="name for the module (filename)")
+parser.add_option("-o", "--objectname", action="store", dest="objectname", help="name for the object (class definition name)")
+(options, args) = parser.parse_args()
+
+class ServerVisitor(ast.NodeVisitor):
+    def __init__(self, objectname):
+        self.objectname = objectname
+        self.functionstofix = {}
+    
+    def generic_visit(self, node):
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_ClassDef(self, node):
+        if node.name == self.objectname:
+            self.getfunctionsofclass(node)
+        self.generic_visit(node)
+    
+    def visit_FunctionDef(self, node):
+        self.editfunctiondef(node)
+        self.generic_visit(node)
+
+    def getfunctionsofclass(self, node):
+        for functiondef in node.body:
+            self.functionstofix[functiondef.lineno] = functiondef
+            
+    def editfunctiondef(self, node):
+        for fname,fnode in ast.iter_fields(node):
+            if fname == 'args':
+                for argname, argnode in ast.iter_fields(fnode):
+                    if argname == 'kwarg' and argnode != None:
+                        del self.functionstofix[node.lineno]
+
+        if node.lineno in self.functionstofix.keys():
+            print "%d | Fixing function definition." % (node.lineno)
+
+def editfile(modulename, objectname):
+    abspath = os.path.abspath(modulename+'.py')
+    # Get the AST tree, find lines to fix
+    astnode = compile(open(abspath, 'rU').read(),"<string>","exec",_ast.PyCF_ONLY_AST)
+    v = ServerVisitor(objectname)
+    v.visit(astnode)
+    functionstofix = v.functionstofix
+    # Get the contents of the file
+    abspath = os.path.abspath(modulename+'.py')
+    objectfile = open(abspath, 'r')
+    filecontent = {}
+    i = 1
+    for line in objectfile:
+        filecontent[i] = line
+        i += 1
+    objectfile.close()
+    # Edit the file
+    for line, function in functionstofix.iteritems():
+        filecontent[line] = string.replace(filecontent[line], "):", ", **kwargs):", 1)
+    objectfile = open(abspath, 'w')
+    for line, content in filecontent.iteritems():
+        objectfile.write(content)
+    objectfile.close()
+    
+def main():
+    editfile(options.modulename, options.objectname)
+        
+if __name__=='__main__':
+    main()
+    
+        
+        
+
+    
