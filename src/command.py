7f411c972af87a0e36b5e5e9660d2de722afa055
Emin Gun Sirer <egs@systems.cs.cornell.edu>
2011-11-26 12:30:16 -0500

    Pass over the Command object

--- src/command.py
+++ src/command.py
@@ -1,12 +1,17 @@
+"""
+@author: denizalti, egs
+@note: A Command is a request from a Client to the replicated object to carry out a method.
+@date: February 1, 2011
+"""
 from threading import Lock, Condition
 
 class Command():
-    """Command encloses a client, clientcommandnumber and command"""
+    """This object represents the command sent by a client"""
     def __init__(self,client=None,clientcommandnumber=0,command=""):
         """Initialize Command
 
         Command State
-        - client
+        - client: the node that initiated this command.
         - clientcommandnumber: unique id for the command, specific to Client
                                doesn't affect paxos commandnumber
         - command: command to be executed
@@ -16,25 +21,18 @@ class Command():
         self.command = command
 
     def __hash__(self):
-        """Returns the hashed command"""
+        # XXX wouldn't it be faster to avoid string creation and disposal by doing
+        # XXX return hash(str(self.client))+hash(self.command) + self.clientcommandnumber
         return hash(str(self.client)+str(self.clientcommandnumber)+str(self.command))
 
     def __eq__(self, othercommand):
-        """Equality function for two Commands.
-        Returns True if given Command is equal to Command, False otherwise.
-        """
+        """Two commands are identical when they're from the same client, the same request, with the same command number."""
         return self.client == othercommand.client and \
             self.clientcommandnumber == othercommand.clientcommandnumber and \
             self.command == othercommand.command
 
     def __ne__(self, othercommand):
-        """Non-equality function for two Commands.
-        Returns True if given Command is not equal to Command, False otherwise.
-        """
-        return self.client != othercommand.client or \
-            self.clientcommandnumber != othercommand.clientcommandnumber or \
-            self.command != othercommand.command
+        return not self.__eq__(othercommand)
     
     def __str__(self):
-        """Returns Command information"""
         return 'Command(%s,%d,%s)' % (str(self.client),self.clientcommandnumber,self.command)
