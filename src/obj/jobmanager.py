b7bbabacf86b9a8e7d4a423459d93ed23fd4dd1e
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-12-01 03:20:31 -0500

    Created the client bundle folder. From this bundle the client can connect to a running instance of ConCoord by giving the bootstrap address. test.py is used to test it. It takes the bootstrap as the argument and runs 3 bank commands.

--- src/obj/jobmanager.py
+++ src/obj/jobmanager.py
@@ -1,5 +1,20 @@
+class Jobmanager():
+    def __init__(self):
+        self.jobs = []
+        
+    def add_job(self, job):
+        self.jobs.append(job)
+        
+    def remove_job(self, job):
+        self.jobs.append(job)
+        
+    def list_jobs(self):
+        return self.__str__()
+        
+    def __str__(self):
+        return " ".join([str(j) for j in self.jobs])
+
 class Job():
-    """Job Object keeping information related to a specific job"""
     def __init__(self, jobname, jobid, jobtime):
         self.name = jobname
         self.id = jobid
@@ -8,30 +23,6 @@ class Job():
     def __str__(self):
         return "Job %s: %s @ %s" % (str(job.id), str(job.name), str(job.time))
 
-class Jobmanager():
-    """Object to keep track of jobs in a system.
-    Supports three functions:
-    - add_job
-    - remove_job
-    - list_jobs
-    """
-    def __init__(self):
-        self.jobs = []
-        
-    def add_job(self, args, **kwargs):
-        job = args[0]
-        self.jobs.append(job)
-        
-    def remove_job(self, args, **kwargs):
-        job = args[0]
-        self.jobs.append(job)
-        
-    def list_jobs(self, **kwargs):
-        return self.__str__()
-        
-    def __str__(self):
-        return " ".join([str(j) for j in self.jobs])
-        
     
         
         
