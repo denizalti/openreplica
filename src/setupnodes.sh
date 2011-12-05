55d6858f83e9a031e54f414ea0f0aed890776846
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-12-05 16:41:57 -0500

    openreplicainitializer is edited to take the objectfilename and classname separately.

--- src/setupnodes.sh
+++ src/setupnodes.sh
@@ -4,9 +4,9 @@ SLICENAME=cornell_openreplica
 NODE=$1
 
 scp -i openreplicakey -o StrictHostKeyChecking=no concoord.tar.gz $SLICENAME@$NODE:
-scp -i openreplicakey -o StrictHostKeyChecking=no dnspython-1.9.4.tar.gz $SLICENAME@$NODE:
-#ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE rm -r bin
+#scp -i openreplicakey -o StrictHostKeyChecking=no dnspython-1.9.4.tar.gz $SLICENAME@$NODE:
+ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE rm -r bin
 ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE mkdir bin
 ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE tar xzf concoord.tar.gz -C bin
-ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE tar xzf dnspython-1.9.4.tar.gz
-ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE 'export SUDO_ASKPASS=/bin/true && sudo -A sh -c "cd dnspython-1.9.4; python setup.py install"'
+#ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE tar xzf dnspython-1.9.4.tar.gz
+#ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE 'export SUDO_ASKPASS=/bin/true && sudo -A sh -c "cd dnspython-1.9.4; python setup.py install"'
