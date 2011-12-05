8b7e61101a9addb052bbc32d50211a3135337ac3
Deniz Altinbuken <deniz@cs.cornell.edu>
2011-12-05 17:03:18 -0500

    testnodes.txt file added to update and clean nodes easily using setupnodes.sh and cleannodes.sh and openreplicainitializer updated to just use filename and assume objectfilecode is saved under that filename

--- src/cleannodes.sh
+++ src/cleannodes.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 SLICENAME=cornell_openreplica
-NODE=$1
 
-ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE 'export SUDO_ASKPASS=/bin/true && sudo -A sh -c "killall -9 python; rm concoord_log*"'
+for var in "$@"
+do
+    echo "$var"
+    ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var 'export SUDO_ASKPASS=/bin/true && sudo -A sh -c "killall -9 python; rm concoord_log*"'
+done
--- src/setupnodes.sh
+++ src/setupnodes.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 SLICENAME=cornell_openreplica
-NODE=$1
 
-scp -i openreplicakey -o StrictHostKeyChecking=no concoord.tar.gz $SLICENAME@$NODE:
-#scp -i openreplicakey -o StrictHostKeyChecking=no dnspython-1.9.4.tar.gz $SLICENAME@$NODE:
-ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE rm -r bin
-ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE mkdir bin
-ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE tar xzf concoord.tar.gz -C bin
-#ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE tar xzf dnspython-1.9.4.tar.gz
-#ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$NODE 'export SUDO_ASKPASS=/bin/true && sudo -A sh -c "cd dnspython-1.9.4; python setup.py install"'
+for var in "$@"
+do
+    echo "$var"
+    scp -i openreplicakey -o StrictHostKeyChecking=no concoord.tar.gz $SLICENAME@$var:
+    #scp -i openreplicakey -o StrictHostKeyChecking=no dnspython-1.9.4.tar.gz $SLICENAME@$var:
+    ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var rm -r bin
+    ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var mkdir bin
+    ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var tar xzf concoord.tar.gz -C bin
+    #ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var tar xzf dnspython-1.9.4.tar.gz
+    #ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var 'export SUDO_ASKPASS=/bin/true && sudo -A sh -c "cd dnspython-1.9.4; python setup.py install"'
+done
