621835a907170f878e98a69cfcd39a2b539742dd
root <root@localhost.localdomain>
2011-12-05 20:22:38 -0500

    changes;

--- src/clientproxyonerun.py
+++ src/clientproxyonerun.py
@@ -24,13 +24,15 @@ REPLY = 0
 CONDITION = 1
 
 class ClientProxy():
-    def __init__(self, bootstrap, debug=False):
+    def __init__(self, bootstrap, debug=True):
         self.debug = debug
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.socket.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
-        self.bootstraplist = []
-        self.discoverbootstrap(bootstrap)
+        self.bootstraplist = self.discoverbootstrap(bootstrap)
+        if len(self.bootstraplist) == 0:
+            print "No bootstrap found"
+            self._graceexit()
         self.connecttobootstrap()
         myaddr = findOwnIP()
         myport = self.socket.getsockname()[1]
@@ -44,33 +46,37 @@ class ClientProxy():
             yield Peer(node[4][0],bootport,NODE_REPLICA)
 
     def _getbootstrapfromdomain(self, domainname):
-        answers = dns.resolver.query('_concoord._tcp.'+domainname, 'SRV')
-        for rdata in answers:
-            for peer in self._getipportpairs(str(rdata.target), rdata.port):
-                yield peer
-            
-    def discoverbootstrap(self, givenbootstrap):
-        bootstrapstrlist = givenbootstrap.split(",")
         try:
-            for bootstrap in bootstrapstrlist:
+            answers = dns.resolver.query('_concoord._tcp.'+domainname, 'SRV')
+            for rdata in answers:
+                for peer in self._getipportpairs(str(rdata.target), rdata.port):
+                    yield peer
+        except dns.resolver.NXDOMAIN:
+            pass
+
+    def discoverbootstrap(self, givenbootstrap):
+        tmpbootstraplist = []
+        try:
+            for bootstrap in givenbootstrap.split(","):
                 # The bootstrap list is read only during initialization
                 if bootstrap.find(":") >= 0:
                     bootaddr,bootport = bootstrap.split(":")
                     for peer in self._getipportpairs(bootaddr, int(bootport)):
-                        self.bootstraplist.append(peer)
+                        tmpbootstraplist.append(peer)
                 else:
-                    self.domainname = bootstrap
+                    self.domainname = bootstrap #XXX side effect
                     for peer in self._getbootstrapfromdomain(self.domainname):
-                        self.bootstraplist.append(peer)
+                        tmpbootstraplist.append(peer)
         except ValueError:
             print "bootstrap usage: ipaddr1:port1,ipaddr2:port2 or domainname"
             self._graceexit()
+        return tmpbootstraplist
 
     def getbootstrapfromdomain(self, domainname):
         tmpbootstraplist = []
         for peer in self._getbootstrapfromdomain(self.domainname):
             tmpbootstraplist.append(peer)
-        self.bootstraplist = tmpbootstraplist
+        return tmpbootstraplist
 
     def connecttobootstrap(self):
         for bootpeer in self.bootstraplist:
@@ -112,9 +118,11 @@ class ClientProxy():
             while not replied:
                 try:
                     success = self.conn.send(cm)
+                    print "Sent message %s" % str(cm)
                     if not success:
                         raise IOError
                     timestamp, reply = self.conn.receive()
+                    print "after receive  %s" % str(reply)
                     if reply and reply.type == MSG_CLIENTREPLY and reply.inresponseto == mynumber:
                         if reply.replycode == CR_REJECTED or reply.replycode == CR_LEADERNOTREADY:
                             raise IOError
