adb1fdb4e68d9e837752e999d980e6579664730d
Emin Gun Sirer <egs@cs.cornell.edu>
2012-01-20 16:08:04 -0500

    cleanup; likely broken

--- src/nameserver.py
+++ src/nameserver.py
@@ -98,6 +98,7 @@ class Nameserver(Tracker):
         response = dns.message.make_response(query)
         for question in query.question:
             self.logger.write("DNS State", "Received Query for %s\n" % question.name)
+
             if (question.rdtype == dns.rdatatype.A or question.rdtype == dns.rdatatype.AAAA) and question.name == self.mydomain:
                 # This is an A Query for my name, I should handle it
                 self.logger.write("DNS State", ">>>>>>>>>>>>>> A Query for my domain: %s" % str(question))
@@ -108,6 +109,7 @@ class Nameserver(Tracker):
                     answerstr += self.create_answer_section(question, addr=address)
                 responsestr = self.create_response(response.id,opcode=dns.opcode.QUERY,rcode=dns.rcode.NOERROR,flags=flagstr,question=question.to_text(),answer=answerstr,authority='',additional='')
                 response = dns.message.from_text(responsestr)
+
             elif question.rdtype == dns.rdatatype.TXT and question.name == self.mydomain:
                 # This is an TXT Query for my name, I should handle it
                 self.logger.write("DNS State", ">>>>>>>>>>>>>> TXT Query for my domain: %s" % str(question))
@@ -117,6 +119,7 @@ class Nameserver(Tracker):
                 answerstr = self.create_answer_section(question, txt=self.txtresponse(question))
                 responsestr = self.create_response(response.id,opcode=dns.opcode.QUERY,rcode=dns.rcode.NOERROR,flags=flagstr,question=question.to_text(),answer=answerstr,authority='',additional='')
                 response = dns.message.from_text(responsestr)
+
             elif question.rdtype == dns.rdatatype.NS and question.name == self.mydomain:
                 # This is an NS Query for my name, I should handle it
                 self.logger.write("DNS State", ">>>>>>>>>>>>>> NS Query for my domain: %s" % str(question)) 
@@ -127,16 +130,18 @@ class Nameserver(Tracker):
                     answerstr += self.create_answer_section(question, addr=address)
                 responsestr = self.create_response(response.id,opcode=dns.opcode.QUERY,rcode=dns.rcode.NOERROR,flags=flagstr,question=question.to_text(),answer=answerstr,authority='',additional='')
                 response = dns.message.from_text(responsestr)
+
             elif question.rdtype == dns.rdatatype.SRV and question.name == self.mydomain:
                 # This is an SRV Query for my name, I should handle it
                 self.logger.write("DNS State", ">>>>>>>>>>>>>> SRV Query for my domain: %s" % str(question)) 
-                flagstr = 'QR AA' # response, authoritative, recursion
+                flagstr = 'QR AA' # response, authoritative
                 answerstr = ''
                 # SRV Queries --> List all Replicas with addr:port
                 for address,port in self.srvresponse(question):
                     answerstr += self.create_srv_answer_section(question, addr=address, port=port)
                 responsestr = self.create_response(response.id,opcode=dns.opcode.QUERY,rcode=dns.rcode.NOERROR,flags=flagstr,question=question.to_text(),answer=answerstr,authority='',additional='')
                 response = dns.message.from_text(responsestr)
+
             else:
                 # This Query is not something I know how to respond to
                 self.logger.write("DNS State", ">>>>>>>>>>>>>> Name Error, %s" %str(question))
@@ -149,42 +154,38 @@ class Nameserver(Tracker):
             print ">>>>>>>>>>>>>>>>>>>>>>>>>>>> Cannot respond to query."
 
     def create_response(self, id, opcode=0, rcode=0, flags='', question='', answer='', authority='', additional=''):
-        responsestr = "id %s\nopcode %s\nrcode %s\nflags %s\n;QUESTION\n%s\n%s%s%s%s%s%s%s%s%s" % (str(id), 
-                                                                                                   OPCODES[opcode], 
-                                                                                                   RCODES[rcode], 
-                                                                                                   flags, 
-                                                                                                   question, 
-                                                                                                   ';ANSWER\n' if answer != '' else '', answer,
-                                                                                                   '\n' if answer != '' else '',
-                                                                                                   ';AUTHORITY\n' if authority != '' else '', authority,
-                                                                                                   '\n' if authority != '' else '',
-                                                                                                   ';ADDITIONAL\n' if additional != '' else '', additional,
-                                                                                                   '\n' if additional != '' else '')
+        answerstr     = ';ANSWER\n'     + answer     + '\n' if answer != '' else ''
+        authoritystr  = ';AUTHORITY\n'  + authority  + '\n' if authority != '' else ''
+        additionalstr = ';ADDITIONAL\n' + additional + '\n' if additional != '' else ''
+
+        responsestr = "id %s\nopcode %s\nrcode %s\nflags %s\n;QUESTION\n%s\n%s%s%s" % (str(id), 
+                                                                                       OPCODES[opcode], 
+                                                                                       RCODES[rcode], 
+                                                                                       flags, 
+                                                                                       question, 
+                                                                                       answerstr, authoritystr, additionalstr)
         return responsestr
 
-    def create_srv_answer_section(self, question, ttl='30', rrclass=1, priority=0, weight=100, port='', addr=''):
-        answerstr = "%s %s %s %s %d %d %d %s\n" % (str(question.name), ttl, RRCLASS[rrclass], RRTYPE[question.rdtype], priority, weight, port, addr)
+    def create_srv_answer_section(self, question, ttl=30, rrclass=1, priority=0, weight=100, port='', addr=''):
+        answerstr = "%s %d %s %s %d %d %d %s\n" % (str(question.name), ttl, RRCLASS[rrclass], RRTYPE[question.rdtype], priority, weight, port, addr)
         return answerstr
 
-    def create_answer_section(self, question, ttl='30', rrclass=1, addr='', name='', txt=None):
-        if question.rdtype == dns.rdatatype.A or dns.rdatatype.TXT:
-            answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[question.rdtype]), str(addr) if txt is None else '"%s"' % txt)
+    def create_answer_section(self, question, ttl=30, rrclass=1, addr='', name='', txt=None):
+        if question.rdtype == dns.rdatatype.A:
+            resp = str(addr)
+        elif dns.rdatatype.TXT:
+            resp = '"%s"' % txt
         elif question.rdtype == dns.rdatatype.NS:
-            answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[question.rdtype]), str(name))
+            resp = str(name)
+        answerstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[question.rdtype]), resp)
         return answerstr
     
-    def create_authority_section(self, question, ttl='30', rrclass=1, rrtype=1, addr='', name=''):
-        if rrtype == dns.rdatatype.A or dns.rdatatype.TXT:
-            authoritystr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(addr))
-        elif rrtype == dns.rdatatype.NS:
-            authoritystr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(name))
+    def create_authority_section(self, question, ttl='30', rrclass=1, rrtype=1, nshost=''):
+        authoritystr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(nshost))
         return authoritystr
 
-    def create_additional_section(self, question, ttl='30', rrclass=1, rrtype=1, addr='', name=''):
-        if rrtype == dns.rdatatype.A or dns.rdatatype.TXT:
-            additionalstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(addr))
-        elif rrtype == dns.rdatatype.NS:
-            additionalstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(name))
+    def create_additional_section(self, question, ttl='30', rrclass=1, rrtype=1, addr=''):
+        additionalstr = "%s %s %s %s %s\n" % (str(question.name), str(ttl), str(RRCLASS[rrclass]), str(RRTYPE[rrtype]), str(addr))
         return additionalstr
 
 def main():
