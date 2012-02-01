5f1f8c825bbdbff0a8bed77381d0299500b81b05
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-02-01 13:17:23 -0500

    Saveopenreplicacoordobj done.

--- src/saveopenreplicacoordobj.py
+++ src/saveopenreplicacoordobj.py
@@ -1,13 +1,33 @@
-import time
-import subprocess
+import os, time
+import md5
 from openreplicacoordobjproxy import OpenReplicaCoordProxy
 
-openreplicacoordobj = OpenReplicaCoordProxy('openreplica.org')
-objectstate = openreplicacoordobj.__str__()
-print "************", objectstate
-f = open('/tmp/'+time.strftime("%Y.%m.%d.%H.%M.%S", time.gmtime())+'-ORobject','w')
-f.write(objectstate)
-f.close()
-#cmd = ['cat', '/tmp/'+time.strftime("%Y.%m.%d.%H.%M.%S", time.gmtime())+'-ORobject']
-#p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#print p.communicate()
+def getlatestfile(filenames):
+    latest = filenames[0]
+    for filename in filenames:
+        if filename > latest:
+            latest = filename
+    return latest
+
+def main():
+    openreplicacoordobj = OpenReplicaCoordProxy('openreplica.org')
+    newobjectstate = openreplicacoordobj.__str__()
+    filenames = []
+    for filename in os.listdir('/tmp/'):
+        if filename.endswith('-ORobject'):
+            filenames.append(filename)
+    oldobjectstate = open('/tmp/'+getlatestfile(filenames),'r').read()
+    m = md5.new()
+    m.update(newobjectstate)
+    newmd5 = m.digest()
+    m = md5.new()
+    m.update(oldobjectstate)
+    oldmd5 = m.digest()
+
+    if oldmd5 != newmd5:
+        f = open('/tmp/'+time.strftime("%Y.%m.%d.%H.%M.%S", time.gmtime())+'-ORobject','w')
+        f.write(newobjectstate)
+        f.close()
+    
+if __name__=='__main__':
+    main()
