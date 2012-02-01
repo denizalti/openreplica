48a91d47efcfc853fac8cca2233c84ee09456a4c
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-02-01 15:09:11 -0500

    script to save openreplica state added. changes made in clientproxy to make calls to objects cleaner, state related print outs are shown only if the debug bit is on.

--- src/saveopenreplicastate.sh
+++ src/saveopenreplicastate.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+if test $# -ne 1; then
+    echo Usage: logmanage filename
+    exit 1
+fi
+FILE=$1
+NEWFILE="$FILE.new"
+python saveopenreplicastate.py > $FILE.new
+if ! cmp -s $NEWFILE $FILE; then
+    NEWNAME=`echo $FILE.\`date '+%Y-%m-%d-%H:%M.%S'\`.txt`
+    mv -f $NEWFILE $NEWNAME && ln -f $NEWNAME $FILE
+fi
