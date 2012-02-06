274f46152e7a581635b13749312d12437ac6bd8e
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-02-06 11:58:46 -0500

    Minor changes.

---
+++ src/cleannodes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SLICENAME=cornell_openreplica
+
+for var in "$@"
+do
+    echo "$var"
+    ssh -i openreplicakey -o StrictHostKeyChecking=no $SLICENAME@$var 'killall -9 python; rm concoord_log*'
+done
