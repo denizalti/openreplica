491616133a2ac95e4df84da684d3af10b7cb0b0b
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-02-07 22:14:57 -0500

    Code clean-up

--- src/setup.py
+++ src/setup.py
@@ -13,6 +13,16 @@ classifiers = [ 'Development Status :: 3 - Alpha'
               , 'Programming Language :: Python'
               ]
 
+ldesc = "ConCoord is a novel coordination service that provides replication and
+	synchronization support for large-scale distributed systems. ConCoord
+	employs an object-oriented approach, in which the system actively
+	creates and maintains live replicas for user-provided objects. Through
+	ConCoord, the clients are able to access these replicated objects
+	transparently as if they are local objects.  The ConCoord approach
+	proposes using these replicated objects to implement coordination
+	constructs in large-scale distributed systems, in effect establishing
+	a transparent way of providing a coordination service."
+
 setup(name='concoord',
       version=concoordversion,
       author='Deniz Altinbuken, Emin Gun Sirer',
@@ -22,7 +32,7 @@ setup(name='concoord',
       license='2-Clause BSD',
       url='http://openreplica.org/',
       description='ConCoord Coordination Service for Distributed Systems',
-      long_description=open('README.txt').read(600),
+      long_description=ldesc,
       classifiers=classifiers,
       
       )
