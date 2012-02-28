6934f8c7228be4c0d04f930cd33667d6b5452c69
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-02-27 23:36:07 -0500

    Barrier object fixed.
    All threading objects require further testing. But
    implementation is done.

---
+++ src/concoord/objects/condition.py
@@ -0,0 +1,50 @@
+"""
+@author: Deniz Altinbuken, Emin Gun Sirer
+@note: Example condition object
+@copyright: See LICENSE
+"""
+from concoord.threadingobjects.dcondition import DCondition
+
+class Condition():
+    """Lock object that supports following functions:
+    - acquire: locks the object
+    - release: unlocks the object
+    """
+    def __init__(self, lock=None, **kwargs):
+        self.condition = DCondition()
+
+    def __repr__(self, **kwargs):
+        return repr(self.condition)
+
+    def acquire(self, **kwargs):
+        try:
+            self.condition.acquire(kwargs)
+        except Exception as e:
+            raise e
+        
+    def release(self, **kwargs):
+        try:
+            self.condition.release(kwargs)
+        except Exception as e:
+            raise e
+        
+    def wait(self, **kwargs):
+        try:
+            self.condition.wait(kwargs)
+        except Exception as e:
+            raise e
+        
+    def notify(self, **kwargs):
+        try:
+            self.condition.notify(kwargs)
+        except Exception as e:
+            raise e
+
+    def notifyAll(self, **kwargs):
+        try:
+            self.condition.notifyAll(kwargs)
+        except Exception as e:
+            raise e
+
+    def __str__(self):
+        return str(self.condition)
