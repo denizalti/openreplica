88dcb5daa0ecd899207f586db734635a093cfe51
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-02-28 17:30:13 -0500

    New example objects are added.

--- src/concoord/objects/membership.py
+++ src/concoord/objects/membership.py
@@ -1,16 +1,21 @@
+"""
+@author: Deniz Altinbuken, Emin Gun Sirer
+@note: Example membership object
+@copyright: See LICENSE
+"""
 class Membership():
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.members = set()
 
-    def add(self, member):
+    def add(self, member, **kwargs):
         if member not in self.members:
             self.members.add(member)
         
-    def remove(self, member):
+    def remove(self, member, **kwargs):
         if member in self.members:
             self.members.remove(member)
         else:
             raise KeyError(member)
         
-    def __str__(self):
+    def __str__(self, **kwargs):
         return " ".join([str(m) for m in self.members])
