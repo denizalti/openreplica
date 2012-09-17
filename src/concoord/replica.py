4e75a5ba4aaf287cdb338d31b636fffd9080452a
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-09-17 18:42:47 -0400

    xput changed to count 5000

--- src/concoord/replica.py
+++ src/concoord/replica.py
@@ -997,17 +997,18 @@ class Replica(Node):
             self.do_command_prepare(givencommand)
             
     def throughput_test(self):
+        XPUTITERS=5000
         self.throughput_runs += 1
         if self.throughput_runs == 100:
             profile_on()
             self.throughput_start = time.time()
-        elif self.throughput_runs == 1100:
+        elif self.throughput_runs == XPUTITERS + 100:
             profile_off()
             self.throughput_stop = time.time()
             totaltime = self.throughput_stop - self.throughput_start
             print "********************************************"
             print "TOTAL: ", totaltime
-            print "TPUT: ", 1000/totaltime, "req/s"
+            print "TPUT: ", XPUTITERS/totaltime, "req/s"
             print "********************************************"
             profilerdict = get_profile_stats()
             for key, value in sorted(profilerdict.iteritems(), key=lambda (k,v): v[1]):
