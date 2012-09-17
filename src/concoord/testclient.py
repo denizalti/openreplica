d7252e11fc434a72ba1345c4a89daf00b399621d
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-09-17 16:54:54 -0400

    client fix

--- src/concoord/testclient.py
+++ src/concoord/testclient.py
@@ -1,10 +1,10 @@
-from test import *
+from concoord.proxy.test import *
 from optparse import OptionParser
 
 parser = OptionParser(usage="usage: %prog -a addr -p port -b bootstrap -f objectfilename -c objectname -n subdomainname -d debug")
 parser.add_option("-b", "--boot", action="store", dest="bootstrap", help="address:port:type triple for the bootstrap peer")
 (options, args) = parser.parse_args()
 
-t = Test("options.bootstrap")
+t = Test(options.bootstrap)
 for i in range(10300):
   t.getvalue()
