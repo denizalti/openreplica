23055abd4c60215d2ac1d8297ce7085125f5e2cb
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-09-30 19:16:34 -0400

    objects and proxies fixed.

--- src/concoord/proxy/membership.py
+++ src/concoord/proxy/membership.py
@@ -1,22 +1,32 @@
 '''
 @author: Deniz Altinbuken, Emin Gun Sirer
-@note: Membership proxy
+@note: Example membership object
 @copyright: See LICENSE
 '''
 from concoord.clientproxy import ClientProxy
+from threading import RLock
 
 class Membership:
     def __init__(self, bootstrap):
-        self.proxy = ClientProxy(bootstrap)
-        
-    def __concoordinit__(self):
+        self.proxy = ClientProxy(bootstrap, token='None')
+
+    def __concoordinit__(self, **kwargs):
         return self.proxy.invoke_command('__init__')
 
-    def add(self, member):
+    def add(self, member, **kwargs):
         return self.proxy.invoke_command('add', member)
 
-    def remove(self, member):
+    def remove(self, member, **kwargs):
         return self.proxy.invoke_command('remove', member)
 
-    def __str__(self):
+    def subscribe(self, **kwargs):
+        return self.proxy.invoke_command('subscribe')
+
+    def unsubscribe(self, **kwargs):
+        return self.proxy.invoke_command('unsubscribe')
+
+    def notifyAll(self):
+        return self.proxy.invoke_command('notifyAll')
+
+    def __str__(self, **kwargs):
         return self.proxy.invoke_command('__str__')
