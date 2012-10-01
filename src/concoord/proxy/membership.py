148b85d3bd1e80eed0dd52289201c5a18269cee9
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-09-30 22:00:46 -0400

    Revert "objects and proxies fixed."
    This reverts commit b92112e84d898601bac5c9d20a1ba9fc4b29efa9.

--- src/concoord/proxy/membership.py
+++ src/concoord/proxy/membership.py
@@ -1,32 +1,22 @@
 '''
 @author: Deniz Altinbuken, Emin Gun Sirer
-@note: Example membership object
+@note: Membership proxy
 @copyright: See LICENSE
 '''
 from concoord.clientproxy import ClientProxy
-from threading import RLock
 
 class Membership:
     def __init__(self, bootstrap):
-        self.proxy = ClientProxy(bootstrap, token='None')
-
-    def __concoordinit__(self, **kwargs):
+        self.proxy = ClientProxy(bootstrap)
+        
+    def __concoordinit__(self):
         return self.proxy.invoke_command('__init__')
 
-    def add(self, member, **kwargs):
+    def add(self, member):
         return self.proxy.invoke_command('add', member)
 
-    def remove(self, member, **kwargs):
+    def remove(self, member):
         return self.proxy.invoke_command('remove', member)
 
-    def subscribe(self, **kwargs):
-        return self.proxy.invoke_command('subscribe')
-
-    def unsubscribe(self, **kwargs):
-        return self.proxy.invoke_command('unsubscribe')
-
-    def notifyAll(self):
-        return self.proxy.invoke_command('notifyAll')
-
-    def __str__(self, **kwargs):
+    def __str__(self):
         return self.proxy.invoke_command('__str__')
