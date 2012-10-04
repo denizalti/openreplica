b1f925fef1bade2a86429272810c94b7c4a61f3b
Deniz Altinbuken <deniz@cs.cornell.edu>
2012-10-04 17:23:56 -0400

    retries are removed

--- src/concoord/test_concoord.sh
+++ src/concoord/test_concoord.sh
@@ -5,5 +5,5 @@ cd ~/projects/concoord/src/ && python setup.py install
 cd ~/projects/concoord/src/concoord && python replica.py -f test_object.py -c Value -p 14000 -a localhost&
 cd ~/projects/concoord/src/concoord && python acceptor.py -b localhost:14000 &
 cd ~/projects/concoord/src/concoord && python nameserver.py -f test_object.py -c Value -n www.test.com -b localhost:14000 -t 1 &
-sleep 2
-cd ~/projects/concoord/src/concoord && python test_client.py -b localhost:14000
+#sleep 2
+#cd ~/projects/concoord/src/concoord && python test_client.py -b localhost:14000
