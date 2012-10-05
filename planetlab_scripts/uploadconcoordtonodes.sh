3b75543509e4f6fcb84cfdceb20c9b23ef6f37e2
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-10-05 13:41:39 -0400

    Updated planetlab scripts

--- planetlab_scripts/uploadconcoordtonodes.sh
+++ planetlab_scripts/uploadconcoordtonodes.sh
@@ -5,6 +5,7 @@ SLICENAME=cornell_openreplica
 for var in "$@"
 do
     echo "$var"
-    scp -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no ../src/dist/concoord-0.2.0.tar.gz $SLICENAME@$var:
-    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'rm -rf concoord-0.2.0; tar xzvf concoord-0.2.0.tar.gz && rm concoord-0.2.0.tar.gz'
+    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'rm -rf concoord*'
+    scp -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no ../src/dist/concoord-0.3.0.tar.gz $SLICENAME@$var:
+    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'tar xzvf concoord-0.3.0.tar.gz && rm concoord-0.3.0.tar.gz'
 done
