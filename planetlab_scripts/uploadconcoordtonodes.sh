470559710eb5c4b6c232ee24a8658304a88b1481
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-10-15 01:16:39 -0400

    Fixed planetlabscript.

--- planetlab_scripts/uploadconcoordtonodes.sh
+++ planetlab_scripts/uploadconcoordtonodes.sh
@@ -8,4 +8,5 @@ do
     ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'rm -rf concoord*'
     scp -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no ../src/dist/concoord-0.3.0.tar.gz $SLICENAME@$var:
     ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'tar xzvf concoord-0.3.0.tar.gz && rm concoord-0.3.0.tar.gz'
+    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'cd concoord-0.3.0/ && /home/cornell_openreplica/python2.7/bin/python2.7 setup.py install'
 done
