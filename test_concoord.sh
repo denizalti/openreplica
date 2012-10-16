cd0cf578f576a39bffcf4a2a6415e568b3d61d02
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-10-15 21:21:19 -0400

    Debugging connection issue

--- test_concoord.sh
+++ test_concoord.sh
@@ -5,5 +5,5 @@ cd src/ && python setup.py install
 cd concoord/ && python replica.py -f test_object.py -c Value -p 14000 -a localhost&
 cd concoord/ && python acceptor.py -b localhost:14000 &
 cd concoord/ && python nameserver.py -f test_object.py -c Value -n www.test.com -b localhost:14000 -t 1 &
-sleep 2
-cd concoord/test/ && python test_client.py -b localhost:14000
+#sleep 2
+#cd concoord/test/ && python test_client.py -b localhost:14000
