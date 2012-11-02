605fbfbc2a976554b80877dc74b3b1d585a634db
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-11-02 13:54:24 -0400

    Fixed view related bug. Fixed bug in openreplicaaddnode. Added the code used in sys machines to reinstantiate an or object from sys machines where we take snapshots regularly.

--- restart_openreplica_locally.sh
+++ restart_openreplica_locally.sh
@@ -5,3 +5,4 @@ cd /var/www/concoord/src/ && python setup.py install
 cd /var/www/concoord/src/concoord && python replica.py -f nameservercoord.py -c NameserverCoord -p 14000 &
 cd /var/www/concoord/src/concoord && python acceptor.py -b 128.84.154.110:14000 &
 cd /var/www/concoord/src/concoord && python openreplica/openreplicanameserver.py -f nameservercoord.py -c NameserverCoord -b 128.84.154.110:14000 -t 1 &
+sleep 10
