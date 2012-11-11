bfa1a7c7d26791c0d9f2699ba87f7335ec07e6c8
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-11-10 23:24:43 -0500

    openreplicaaddnode simplified and cleaned up. It follows openreplicainitializer now.

--- src/concoord/openreplica/openreplicaaddnode.py
+++ src/concoord/openreplica/openreplicaaddnode.py
@@ -32,44 +32,31 @@ try:
 except:
     NPYTHONPATH = 'python'
 
-def terminated(p):
-    i = 5
-    done = p.poll() is not None
-    while not done and i>0:
-        sleep(1)
-        i -= 1
-        done = p.poll() is not None
-    return done
-
 # checks if a PL node is suitable for running a nameserver
 def check_planetlab_dnsport(plconn, node):
-    print ("."),
     pathtodnstester = CONCOORD_HELPERDIR+'testdnsport.py'
     plconn.uploadone(node, pathtodnstester)
     terminated, output = plconn.executecommandone(node, "sudo "+NPYTHONPATH+" testdnsport.py")
     success = terminated and output[STDERR] == ''
     plconn.executecommandone(node, "rm testdnsport.py")
-    print ("."),
     return success,output
 
 def check_planetlab_pythonversion(plconn, node):
-    print ("."),
     pathtopvtester = CONCOORD_HELPERDIR+'testpythonversion.py' 
     plconn.uploadone(node, pathtopvtester)
     terminated, output = plconn.executecommandone(node, NPYTHONPATH + " testpythonversion.py")
     success = terminated and output[STDERR] == ''
     plconn.executecommandone(node, "rm testpythonversion.py")
-    print ("."),
     return success,output
 
 def get_startup_cmd(nodetype, subdomain, node, port, clientobjectfilename, classname, bootstrapname, servicetype, master):
     startupcmd = ''
     if nodetype == NODE_REPLICA:
-        startupcmd = "nohup " + NPYTHONPATH + " " + CONCOORDPATH + "replica.py -a %s -p %d -f %s -c %s -b %s -l %s &" % (node, port, clientobjectfilename, classname, bootstrapname, LOGGERNODE)
+        startupcmd = "nohup " + NPYTHONPATH + " " + CONCOORDPATH + "replica.py -a %s -p %d -f %s -c %s -b %s -l %s > foo.out 2> foo.err < /dev/null &" % (node, port, clientobjectfilename, classname, bootstrapname, LOGGERNODE)
     elif nodetype == NODE_ACCEPTOR:
-        startupcmd = "nohup " + NPYTHONPATH + " " + CONCOORDPATH + "acceptor.py -a %s -p %d -f %s -b %s -l %s &" % (node, port, clientobjectfilename, bootstrapname, LOGGERNODE)
+        startupcmd = "nohup " + NPYTHONPATH + " " + CONCOORDPATH + "acceptor.py -a %s -p %d -f %s -b %s -l %s > foo.out 2> foo.err < /dev/null &" % (node, port, clientobjectfilename, bootstrapname, LOGGERNODE)
     elif nodetype == NODE_NAMESERVER:
-        startupcmd =  "nohup " + NPYTHONPATH + " " + CONCOORDPATH + "nameserver.py -n %s -a %s -p %d -f %s -c %s -b %s -t %d -m %s -l %s &" % (subdomain+'.openreplica.org', node, port, clientobjectfilename, classname, bootstrapname, servicetype, master, LOGGERNODE)
+        startupcmd =  "nohup " + NPYTHONPATH + " " + CONCOORDPATH + "nameserver.py -n %s -a %s -p %d -f %s -c %s -b %s -t %d -m %s -l %s > foo.out 2> foo.err < /dev/null &" % (subdomain+'.openreplica.org', node, port, clientobjectfilename, classname, bootstrapname, servicetype, master, LOGGERNODE)
     return startupcmd
         
 def start_node(nodetype, subdomain, clientobjectfilepath, classname, bootstrapname):
@@ -78,30 +65,30 @@ def start_node(nodetype, subdomain, clientobjectfilepath, classname, bootstrapna
     master = 'openreplica.org'
     print "\n==== Adding %s ====" % node_names[nodetype]
     clientobjectfilename = os.path.basename(clientobjectfilepath)
-    print ("Picking node..."),
+    print "Picking node..."
     if nodetype == NODE_NAMESERVER:
         nodeconn = PLConnection(1, [check_planetlab_dnsport, check_planetlab_pythonversion], configdict=CONFIGDICT)
     else:
         nodeconn = PLConnection(1, [check_planetlab_pythonversion], configdict=CONFIGDICT)
-    
-    failure = True
-    trials = 0
-    while failure and trials < 5:
-        node = nodeconn.getHosts()[0]
-        print "\nPicked Node: %s" % node
+    node = nodeconn.getHosts()[0]
+    print "Picked Node: %s" % node
 
+    terminated = True
+    numtries = 0
+    while terminated and numtries < 5:
         if nodetype != NODE_ACCEPTOR:
             nodeconn.uploadall(clientobjectfilepath, CONCOORDPATH + clientobjectfilename)
         port = random.randint(14000, 15000)
         p = nodeconn.executecommandone(node, get_startup_cmd(nodetype, subdomain, node, port,
                                                              clientobjectfilename, classname,
                                                              bootstrapname, servicetype, master), False)
+        terminated, output = nodeconn.executecommandone(node, 'cat foo.out')
+        terminated = terminated and output[STDOUT] != ''
+        nodeconn.executecommandone(node, 'rm foo*')
         nodename = node + ':' + str(port)
-        # the start-up failed is p terminates
-        failure = terminated(p)
-        trials += 1
+        numtries += 1
 
-    if failure:
+    if terminated:
         print "Adding node FAILED. Please try again."
         return
 
@@ -112,7 +99,7 @@ def start_node(nodetype, subdomain, clientobjectfilepath, classname, bootstrapna
     print "Node is started: %s" % nodename
 
 def main():
-    print ("Connecting..."),
+    print "Starting nodes..."
     start_node(options.nodetype, options.subdomain, options.objectfilepath,
                options.classname, options.bootstrapname)
     
