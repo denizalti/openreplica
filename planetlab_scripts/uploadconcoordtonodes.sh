5def48d5a4d8b433ff57efb1b7906e605139459e
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2012-12-02 22:28:12 -0500

    New sources uploaded to planetlab nodes and changes made accordingly.

--- planetlab_scripts/uploadconcoordtonodes.sh
+++ planetlab_scripts/uploadconcoordtonodes.sh
@@ -6,7 +6,7 @@ for var in "$@"
 do
     echo "$var"
     ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'rm -rf concoord*'
-    scp -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no ../src/dist/concoord-0.3.0.tar.gz $SLICENAME@$var:
-    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'tar xzvf concoord-0.3.0.tar.gz && rm concoord-0.3.0.tar.gz'
-    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'cd concoord-0.3.0/ && /home/cornell_openreplica/python2.7/bin/python2.7 setup.py install'
+    scp -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no ../src/dist/concoord-0.9.0.tar.gz $SLICENAME@$var:
+    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'tar xzvf concoord-0.9.0.tar.gz && rm concoord-0.9.0.tar.gz'
+    ssh -i ../secret/openreplicaplkey -o StrictHostKeyChecking=no $SLICENAME@$var 'cd concoord-0.9.0/ && /home/cornell_openreplica/python2.7/bin/python2.7 setup.py install'
 done
