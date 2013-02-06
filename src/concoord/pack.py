089c99d8e173133c970cae5372b3bc8d7d38d703
Deniz Altinbuken <deniz@cs.cornell.edu>
2013-02-06 11:17:16 -0500

    Group object is removed and replaced with regular dictionaries.

--- src/concoord/pack.py
+++ src/concoord/pack.py
@@ -4,10 +4,12 @@
 @copyright: See LICENSE
 """
 
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 
 Proposal = namedtuple('Proposal', ['client', 'clientcommandnumber', 'command'])
 
 Peer = namedtuple('Peer', ['addr', 'port', 'type'])
 def getpeerid(peer):
     return '%s:%d' % (peer.addr, peer.port)
+
+
