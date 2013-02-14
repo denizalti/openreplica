af30ebe2582d066d490fbb973e28a4a5cad70413
Deniz Altinbuken <deniz@cs.cornell.edu>
2013-02-13 23:11:52 -0500

    Receiver thread is added to node.py

--- src/concoord/node.py
+++ src/concoord/node.py
@@ -63,6 +63,9 @@ class Node():
                 self._graceexit(1)
             self.objectfilename = objectfilename
             self.objectname = objectname
+        ## initialize receive queue
+        self.receivedmessages_semaphore = Semaphore(0)
+        self.receivedmessages = []
         # lock to synchronize message handling
         self.lock = Lock()
         # create server socket and bind to a port
@@ -173,8 +176,11 @@ class Node():
             time.sleep(1)
 
     def startservice(self):
+        # Start a thread that waits for inputs
+        receiver_thread = Thread(target=self.server_loop, name='ReceiverThread')
+        receiver_thread.start()
         # Start a thread with the server which will start a thread for each request
-        main_thread = Thread(target=self.server_loop, name='MainThread')
+        main_thread = Thread(target=self.handle_messages, name='MainThread')
         main_thread.start()
         # Start a thread that waits for inputs
         input_thread = Thread(target=self.get_user_input_from_shell, name='InputThread')
@@ -289,11 +295,13 @@ class Node():
             if message.type == MSG_STATUS:
                 if self.type == NODE_REPLICA:
                     self.logger.write("State", "Answering status message %s" % self.__str__())
-                    # XXX
                     messagestr = pickle.dumps(self.__str__())
                     message = struct.pack("I", len(messagestr)) + messagestr
                     clientsock.send(message)
                 return
+            # add to receivedmessages
+            self.receivedmessages.append((message,connection))
+            self.receivedmessages_semaphore.release()
             if message.type == MSG_CLIENTREQUEST or message.type == MSG_INCCLIENTREQUEST:
                 try:
                     self.clientpool.add_connection_to_peer(message.source, connection)
@@ -301,19 +309,26 @@ class Node():
                     pass
             else:
                 self.connectionpool.add_connection_to_peer(message.source, connection)
-            # process the message received
-            self.process_message(message,connection)
         return True
+
+    def handle_messages(self):
+        while True:
+            self.receivedmessages_semaphore.acquire()
+            if self.type == NODE_REPLICA and len(self.pendingmetacommands) > 0:
+                # A node should be removed from the view
+                with self.pendingmetalock:
+                    self.pendingmetacommands = set()
+                self.initiate_command()
+            try:
+                (message_to_process, connection) = self.receivedmessages.pop(0)
+                self.process_message(message_to_process, connection)
+            except:
+                continue
+        return
 
     def process_message(self, message, connection):
         """Process message loop that takes messages out of
         the receivedmessages list and handles them."""
-        if self.type == NODE_REPLICA and len(self.pendingmetacommands) > 0:
-            # a node should be removed from the view
-            with self.pendingmetalock:
-                self.pendingmetacommands = set()
-            self.initiate_command()
-
         # find method and invoke it holding a lock
         mname = "msg_%s" % msg_names[message.type].lower()
         try:
