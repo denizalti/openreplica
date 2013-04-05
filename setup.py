37c1aa630dd9700aae5cb5c9f36ac8cbe0bce097
Deniz Altinbuken <deniz@cs.cornell.edu>
2013-04-05 12:58:53 -0400

    setup.py before merge.

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
     entry_points={
         'console_scripts': [
             'concoord=concoord.main:main',
+            'concoordify=concoord.main:main_concoordify',
         ]
     },
     install_requires=[
