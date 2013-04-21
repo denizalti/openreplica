0560415b4ee2dd11d24702c58f55c9ff99b502ca
Deniz Altinbuken <deniz@systems.cs.cornell.edu>
2013-04-21 16:17:33 -0400

    setup.py and script writing for simplicity and correctness.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ from setuptools import find_packages
 v = open(os.path.join(os.path.dirname(__file__), 'concoord', '__init__.py'), 'r')
 VERSION = re.match(r".*__version__ = '(.*?)'", v.read(), re.S).group(1)
 
+print find_packages()
 
 classifiers = [
     'Development Status :: 3 - Alpha',
@@ -22,17 +23,6 @@ classifiers = [
     'Programming Language :: Python :: 2.7',
 ]
 
-
-ldesc="""ConCoord is a novel coordination service that provides replication and
-synchronization support for large-scale distributed systems. ConCoord employs an
-object-oriented approach, in which the system actively creates and maintains
-live replicas for user-provided objects. Through ConCoord, the clients areable
-to access these replicated objects transparently as if they are local objects.
-The ConCoord approach proposes using these replicated objects to implement
-coordination constructs in large-scale distributed systems, in effect
-establishing a transparent way of providing a coordination service."""
-
-
 setup(
     name='concoord',
     version=VERSION,
@@ -43,13 +33,10 @@ setup(
     license='3-Clause BSD',
     url='http://openreplica.org/',
     description='Coordination service for distributed systems.',
-    long_description=ldesc,
+    long_description=open('README').read(),
     classifiers=classifiers,
     entry_points={
-        'console_scripts': [
-            'concoord=concoord.main:main',
-            'concoordify=concoord.main:main_concoordify',
-        ]
+        'console_scripts': ['concoord = concoord.main:main',]
     },
     install_requires=[
         'msgpack-python',
